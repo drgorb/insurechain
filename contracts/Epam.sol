4901eb42f14d9a55eff62e1e16605567e3c85bf0
adridadou <david@roon.me>
2016-11-18 21:43:48 +0100

    first version of the smart contract

---
+++ contracts/Epam.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.4;
+
+contract PriceCalculator {
+
+	function getPrice(string manufacturer, string productType, uint price) constant returns(uint) {
+		return price * 5 / 100;
+	}
+}
+
+contract Epam {
+
+	struct Product {
+		string manufacturer;
+		string productType;
+		address owner;
+		string warrantyId;
+		uint warrantyEndDate;
+	}
+
+	address owner;
+	mapping(string => Product) products;
+	PriceCalculator calculator;
+
+	modifier ownerOnly {
+		if(msg.sender != owner) {
+			throw;
+		}
+		_;
+	}
+
+	modifier noWarranty(string serial) {
+		if(products[serial].warrantyEndDate > now) {
+			throw;
+		}
+		_;
+	}
+
+	function updateCalculator(address contractAddress) ownerOnly {
+		calculator = contractAddress;
+	}
+
+	function requestWarranty(string serial, address owner, uint endDate) noWarranty(serial) {
+		products[serial].owner = owner;
+		products[serial].warrantyEndDate = endDate;
+
+	}
+
+}
