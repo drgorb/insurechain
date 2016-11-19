c1a15ecae00c6a95a6db862672180c7fcb85952b
Micha Roon <micha.roon@managination.com>
2016-11-19 10:24:30 +0100

    update

--- app/src/EthereumService.js
+++ app/src/EthereumService.js
@@ -8,12 +8,50 @@ function EthereumService($q) {
 
     var self = this;
 
-    self.abi = [ { "constant": true, "inputs": [ { "name": "serial", "type": "string" } ], "name": "getCustomer", "outputs": [ { "name": "", "type": "address", "value": "0x0000000000000000000000000000000000000000" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "serial", "type": "string" }, { "name": "customer", "type": "address" }, { "name": "endDate", "type": "uint256" } ], "name": "requestWarranty", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "contractAddress", "type": "address" } ], "name": "updateCalculator", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "serial", "type": "string" } ], "name": "getEndDate", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "serial", "type": "string" } ], "name": "isWarrantyValid", "outputs": [ { "name": "", "type": "bool", "value": true } ], "payable": false, "type": "function" }, { "inputs": [], "type": "constructor" } ];
+    self.abi = [{
+        "constant": true,
+        "inputs": [{"name": "serial", "type": "string"}],
+        "name": "getCustomer",
+        "outputs": [{"name": "", "type": "address", "value": "0x0000000000000000000000000000000000000000"}],
+        "payable": false,
+        "type": "function"
+    }, {
+        "constant": false,
+        "inputs": [{"name": "serial", "type": "string"}, {"name": "customer", "type": "address"}, {
+            "name": "endDate",
+            "type": "uint256"
+        }],
+        "name": "requestWarranty",
+        "outputs": [],
+        "payable": false,
+        "type": "function"
+    }, {
+        "constant": false,
+        "inputs": [{"name": "contractAddress", "type": "address"}],
+        "name": "updateCalculator",
+        "outputs": [],
+        "payable": false,
+        "type": "function"
+    }, {
+        "constant": true,
+        "inputs": [{"name": "serial", "type": "string"}],
+        "name": "getEndDate",
+        "outputs": [{"name": "", "type": "uint256", "value": "0"}],
+        "payable": false,
+        "type": "function"
+    }, {
+        "constant": true,
+        "inputs": [{"name": "serial", "type": "string"}],
+        "name": "isWarrantyValid",
+        "outputs": [{"name": "", "type": "bool", "value": true}],
+        "payable": false,
+        "type": "function"
+    }, {"inputs": [], "type": "constructor"}];
     self.dappId = "epam.hackathon";
 
     self.contractAddress = "0xF65a613630e112cc2Fc0fB382770E3845b857105";
     self.walletBar = new WalletBar({
-        containerName:'#signInId',
+        containerName: '#signInId',
         dappNamespace: self.dappId,
         blockchain: "morden"
     });
@@ -21,97 +59,191 @@ function EthereumService($q) {
     self.web3 = new Web3();
     self.WarrantyContract;
     self.waitForWallet = self.walletBar.applyHook(self.web3)
-    .then(function() {
-        self.WarrantyContract = self.web3.eth.contract(self.abi).at(self.contractAddress);
-        return self.WarrantyContract;
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+        .then(function () {
+            self.WarrantyContract = self.web3.eth.contract(self.abi).at(self.contractAddress);
+            return self.WarrantyContract;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 
     function prepareForTransaction() {
-      var account = self.walletBar.getCurrentAccount(); // get account selected in wallet bar
-      if (!account) return alert("You must log in to transact");
-      self.walletBar.createSecureSigner();
-      return account;
+        var account = self.walletBar.getCurrentAccount(); // get account selected in wallet bar
+        if (!account) return alert("You must log in to transact");
+        self.walletBar.createSecureSigner();
+        return account;
     }
 
-    self.getInfo = function(serial) {
-      return self.waitForWallet.then(function() {
-       return $q.all({
-          isWarrantyValid: self.isWarrantyValid(serial),
-          customer: self.getCustomer(serial),
-          warrantyEndDate: self.getWarrantyEndDate(serial)
-        });
-     });
-    }
-
-    self.getReportData = function(month) {
-        return $q(function(resolve, reject) {
-            setTimeout(function() {
-                resolve("productType,productName,serial,pricePaid,claim,claimAmount,claimantId");
-            }, 500);
+    self.getInfo = function (serial) {
+        return self.waitForWallet.then(function () {
+            return $q.all({
+                isWarrantyValid: self.isWarrantyValid(serial),
+                customer: self.getCustomer(serial),
+                warrantyEndDate: self.getWarrantyEndDate(serial)
+            });
         });
     }
 
-    self.isWarrantyValid = function(serial) {
-      console.log('is warranty valid');
-      var defer = $q.defer();
+    self.getReportData = function (month) {
+        return $q.all({
+            requests: self.getRequestCount(),
+            claims: self.getClaimCount()
+        }).then(function (counts) {
+            var getClaims = [];
+            for (var i = 0; i < counts.claims; i++) {
+                getClaims.push(self.WarrantyContract.getClaim(idx, function (err, result) {
+                        if (err) {
+                            defer.reject(err);
+                        } else {
+                            defer.resolve(result);
+                        }
+                    })
+                )
+            }
+            var getRequests = [];
+            for (var i = 0; i < counts.requests; i++) {
+                getClaims.push(self.WarrantyContract.getRequest(idx, function (err, result) {
+                        if (err) {
+                            defer.reject(err);
+                        } else {
+                            defer.resolve(result);
+                        }
+                    })
+                )
+            }
+            return $q.all({
+                claims: getClaims,
+                requests: getRequests
+            });
+        }).then(function(results){
+            var report = "productType,productName,serial,pricePaid,claimDate,claimAmount,claimantId\n";
+            results.claims.forEach(function (claim) {
+                report += "," //productType
+                report += "," //productName
+                report += claim.serial + "," //productSerial
+            });
+            return $q(function (resolve, reject) {
+                resolve(report);
+            });
+        });
 
-      self.WarrantyContract.isWarrantyValid(serial, function(err, result){
-        if(err) {
-          defer.reject(err);
-        }else {
-          defer.resolve(result);
-        }
-      });
 
-      return defer.promise;
     }
 
-    self.getCustomer = function(serial) {
-      var defer = $q.defer();
+    self.isWarrantyValid = function (serial) {
+        console.log('is warranty valid');
+        var defer = $q.defer();
 
-      self.WarrantyContract.getCustomer(serial, function(err, result){
-        if(err) {
-          defer.reject(err);
-        }else {
-          defer.resolve(result);
-        }
-      });
+        self.WarrantyContract.isWarrantyValid(serial, function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
 
-      return defer.promise;
+        return defer.promise;
     }
 
-    self.getWarrantyEndDate = function(serial) {
-      var defer = $q.defer();
+    self.getCustomer = function (serial) {
+        var defer = $q.defer();
 
-      self.WarrantyContract.getEndDate(serial, function(err, result){
-        if(err) {
-          defer.reject(err);
-        }else {
-          defer.resolve(result);
-        }
-      });
+        self.WarrantyContract.getCustomer(serial, function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
 
-      return defer.promise;
+        return defer.promise;
     }
 
-    self.requestWarranty = function(serial, owner, endDate) {
-      var defer = $q.defer();
-      var account = prepareForTransaction();
-      self.WarrantyContract.requestWarranty(serial, owner, endDate.getTime(),{from:account}, function(err, result){
-        if(err) {
-          defer.reject(err);
-        }else {
-          defer.resolve(result);
-        }
-      });
+    self.getRequestCount = function () {
+        var defer = $q.defer();
 
-      return defer.promise;
+        self.WarrantyContract.getRequestCount(function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+
+        return defer.promise;
+    }
+
+    self.getClaimsCount = function () {
+        var defer = $q.defer();
+
+        self.WarrantyContract.getClaimsCount(function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+
+        return defer.promise;
+    }
+
+    self.getRequest = function (idx) {
+        var defer = $q.defer();
+
+        self.WarrantyContract.getRequest(idx, function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+
+        return defer.promise;
+    }
+
+    self.getClaim = function (idx) {
+        var defer = $q.defer();
+
+        self.WarrantyContract.getClaim(idx, function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+
+        return defer.promise;
+    }
+
+    self.getWarrantyEndDate = function (serial) {
+        var defer = $q.defer();
+
+        self.WarrantyContract.getEndDate(serial, function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+
+        return defer.promise;
+    }
+
+    self.requestWarranty = function (serial, owner, endDate) {
+        var defer = $q.defer();
+        var account = prepareForTransaction();
+        self.WarrantyContract.requestWarranty(serial, owner, endDate.getTime(), {from: account}, function (err, result) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+
+        return defer.promise;
     }
 
     return self;
 }
 
-export default ['$q', EthereumService ];
+export default ['$q', EthereumService];
