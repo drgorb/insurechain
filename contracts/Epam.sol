6935c4d315414a1da96b256472c0b75aef4955b3
adridadou <david@roon.me>
2016-11-19 09:52:23 +0100

    struggling with return types

--- contracts/Epam.sol
+++ contracts/Epam.sol
@@ -23,12 +23,20 @@ contract Epam {
 		address claimant;
 		uint amount;
 		string serial;
+		uint date;
+	}
+
+	struct Request {
+		string serial;
+		uint date;
 	}
 
 	address owner;
 	mapping(string => Product) products;
 	mapping(uint => Claim) claims;
+	mapping(uint => Request) requests;
 	uint claimCount;
+	uint requestCount;
 
 	PriceCalculator calculator;
 
@@ -55,21 +63,27 @@ contract Epam {
 	}
 
 	function requestWarranty(string serial, address customer, uint endDate, uint price) noWarranty(serial) {
-		Product memory product;
+		Product product = products[serial];
 		product.retailer = msg.sender;
 		product.customer = customer;
 		product.warrantyEndDate = endDate;
 		product.price = price;
 
 		products[serial] = product;
+
+		Request request = requests[requestCount];
+		request.date = now;
+		request.serial = serial;
+		requestCount++;
 	}
 
 	function claimWaranty(string serial, uint amount) {
-		Claim memory claim;
+		Claim claim = claims[claimCount];
 		claim.claimant = msg.sender;
 		claim.amount = amount;
 		claim.serial = serial;
-		claims[claimCount++] = claim;
+		claim.date = now;
+		claimCount++;
 	}
 
 	function isWarrantyValid(string serial) constant returns (bool) {
@@ -82,6 +96,24 @@ contract Epam {
 
 	function getCustomer(string serial) constant returns (address) {
 		return products[serial].customer;
+	}
+
+	function getClaimCount() constant returns (uint) {
+		return claimCount;
+	}
+
+	function getClaim(uint id) constant returns (address, uint, string, uint) {
+		return (claims[id].claimant, claims[id].amount, claims[id].serial, claims[id].date);
+	}
+
+	function getRequestCount() constant returns (uint) {
+		return requestCount;
+	}
+
+
+	function getRequest(uint id) constant returns (uint, string, address, string, address, string, uint, uint) {
+		Product product = products[requests[id].serial];
+		return (requests[id].date, requests[id].serial, product.manufacturer, product.productType, product.customer, product.warrantyId, product.warrantyEndDate, product.price);
 	}
 
 }
