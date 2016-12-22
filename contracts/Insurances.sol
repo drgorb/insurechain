c306e8ffc6bbec87dcc61a4a21d99783a6228063
Micha Roon <micha.roon@managination.com>
2016-12-22 23:09:01 +0100

    remove imports

--- contracts/Insurances.sol
+++ contracts/Insurances.sol
@@ -1,10 +1,28 @@
 pragma solidity ^0.4.4;
 
-import "./Mortal.sol";
-import './Retailers.sol';
+contract owned {
+    address owner;
+
+    modifier ownerOnly() {
+        if (msg.sender == owner) _;
+        throw;
+    }
+
+    function Owned() {
+        owner = msg.sender;
+    }
+}
+
+contract mortal is owned {
+    enum Status {Requested, Accepted, Rejected, Terminated}
+
+    function kill() {
+        if (msg.sender == owner) selfdestruct(owner);
+    }
+}
 
 contract Insurances is mortal {
-    
+
     struct PartnerRelations {
         address retailer;
         Status status;
