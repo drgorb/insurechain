7dec94755b95ef57a6616a169d1ed222cd844d57
Lukasz Zygmanski <lukasz_zygmanski@epam.com>
2016-12-22 17:54:27 +0100

    add adresses form github readme

--- src/modules/ethereum/Retailers.js
+++ src/modules/ethereum/Retailers.js
@@ -58,48 +58,33 @@ function RetailersEthereumService($q) {
     self.insauranceList = [
         {
             name: 'Zurich insurance',
-            address: '1'
+            address: '0x95262f78c646178416c123dbeef2a286d41a27e8'
         },
         {
             name: 'Allianz',
-            address: '2'
+            address: '0xc62e02ddc6c1a78ca63f144253e74c85ecb76b74'
         },
         {
-            name: 'Vaudoise',
-            address: '3'
+            name: 'Mobiliar',
+            address: '0x607aae63a7d99e0207214248b9f663e55b465766'
         }
     ]
 
     self.retailerList = [
         {
-            name: 'Company name 1',
-            address: '1',
+            name: 'Digitec',
+            address: '0x3b06274c18e8a188c24f64dd4793f1027c1f3123',
             status: 0
         },
         {
-            name: 'Company name 2',
-            address: '2',
+            name: 'Interdiscount',
+            address: '0xce2db460f2c86b5b66bfb2815d5ee476e61584ac',
             status: 2
         },
         {
-            name: 'Company name 3',
-            address: '3',
+            name: 'M-Electronics',
+            address: '0xb3b499b3a1f35d1b413e8fb17d0134ed11c650cc',
             status: 0
-        },
-        {
-            name: 'Company name 4',
-            address: '4',
-            status: 2
-        },
-        {
-            name: 'Company name 5',
-            address: '5',
-            status: 2
-        },
-        {
-            name: 'Company name 6',
-            address: '6',
-            status: 3
         }
     ]
 
