6627367c6981dece6910c83ad2d7e8954742ff16
Lukasz Zygmanski <lukasz_zygmanski@epam.com>
2016-12-22 14:45:35 +0100

    ng-message and validation

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,7 @@ import uirouter from "angular-ui-router";
 import ngMaterial from "angular-material";
 import ngMdIcons from "angular-material-icons";
 import ngMaterialSidemenu from "angular-material-sidemenu";
+import ngMessages from 'angular-messages'
 import "!style!css!angular-material-sidemenu/dest/angular-material-sidemenu.css";
 import "../style/app.scss";
 import homeModule from "../modules/home/Home";
@@ -15,25 +16,26 @@ import AppController from "./controllers/AppController";
 const appModule = 'app';
 
 angular.module(appModule, [
-  uirouter,
-  ngMaterial,
-  ngMdIcons,
-  ngMaterialSidemenu,
-  homeModule,
-  productModule,
-  retailerModule,
-  warrantyModule,
-  insuranceModule
+    uirouter,
+    ngMessages,
+    ngMaterial,
+    ngMdIcons,
+    ngMaterialSidemenu,
+    homeModule,
+    productModule,
+    retailerModule,
+    warrantyModule,
+    insuranceModule,
 ])
-.controller('AppController', AppController)
-.config(['$locationProvider', '$urlRouterProvider', function($locationProvider, $urlRouterProvider) {
-  $locationProvider.html5Mode(true);
-  $urlRouterProvider.otherwise('/');
-}])
-.config(($mdThemingProvider) => {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('brown')
-    .accentPalette('red');
-});
+    .controller('AppController', AppController)
+    .config(['$locationProvider', '$urlRouterProvider', function($locationProvider, $urlRouterProvider) {
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise('/');
+    }])
+    .config(($mdThemingProvider) => {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('brown')
+            .accentPalette('red');
+    });
 
 export default appModule;
