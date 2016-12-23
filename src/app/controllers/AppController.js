47871f48991b5760d924da97728645f145a4f13e
Micha Roon <micha.roon@managination.com>
2016-12-23 07:12:16 +0100

    clean up web3 and wait for injection

--- src/app/controllers/AppController.js
+++ src/app/controllers/AppController.js
@@ -1,9 +1,5 @@
 function AppController($scope, $mdSidenav, $location) {
 
-    $scope.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
-    };
-
     $scope.menu = [
         {
             link : 'home',
@@ -32,11 +28,29 @@ function AppController($scope, $mdSidenav, $location) {
         }
     ];
 
-    $scope.user = web3.eth.accounts[0] || 'No active user';
+    window.addEventListener('load', function() {
 
-    $scope.isActive = function(route) {
-        return route === $location.path();
-    }
+        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+        if (typeof web3 !== 'undefined') {
+            // Use Mist/MetaMask's provider
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            console.log('No web3? You should consider trying MetaMask!')
+            // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        }
+
+        $scope.toggleSidenav = function(menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+
+        $scope.user = web3.eth.accounts[0] || 'No active user';
+
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        }
+
+    })
 }
 
 export default ['$scope','$mdSidenav', '$location', AppController]
