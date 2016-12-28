edc9eea8d01244b99e37e66b44604684f55b090e
Micha Roon <micha.roon@managination.com>
2016-12-28 23:37:18 +0100

    added bytecode to contract-defintition

--- src/test/pudding/insurechain-contract-tests.js
+++ src/test/pudding/insurechain-contract-tests.js
@@ -1,5 +1,5 @@
 var fs = require ("fs");
-var crypto = require('crypto');
+var crypto = require ('crypto');
 var Web3 = require ("web3");
 var _ = require ("lodash");
 var $q = require ("q");
@@ -7,7 +7,7 @@ var $q = require ("q");
 var web3 = new Web3 ();
 web3.setProvider (new web3.providers.HttpProvider ('http://localhost:8646'));
 
-console.log(web3.eth.getCompilers());
+console.log (web3.eth.getCompilers ());
 
 var soliditySources = {
     insurechain: (fs.readFileSync ('contracts/Insurechain.sol') + ' '),
@@ -19,52 +19,52 @@ var compiledContracts = {};
 
 _.each (soliditySources, (source, name) => {
     compiledContracts[name] = web3.eth.compile.solidity (source);
-    compiledContracts[name].contract = web3.eth.contract(compiledContracts[name].info.abiDefinition);
-    var defer = $q.defer();
-    compiledContracts[name].contract.new({
+    compiledContracts[name].contract = web3.eth.contract (compiledContracts[name].info.abiDefinition);
+    var defer = $q.defer ();
+    compiledContracts[name].contract.new ({
             from: "0x6d8B18F9b737160A73F536393C908FE89961E570",
             data: compiledContracts[name].code,
             value: 0,
             gas: 1000000
         },
-        function(err, contract) {
+        function (err, contract) {
             if (err) {
-                defer.reject(err);
+                defer.reject (err);
             } else {
                 /*the callback is called twice, the second time with an address*/
-                if(contract.address){
+                if (contract.address) {
                     contract.name = name;
-                    defer.resolve(contract);
+                    defer.resolve (contract);
                 }
             }
         });
-    contractTransactions.push(defer.promise)
+    contractTransactions.push (defer.promise)
 });
 
-$q.all(contractTransactions)
-    .then(function (results) {
+$q.all (contractTransactions)
+    .then (function (results) {
         var json = "";
         var exports = "export {x";
-        results.forEach(function (result) {
+        results.forEach (function (result) {
             if (result.name) {
-                /*TODO: if the hash is the same as the existing contract, do not overwrite*/
-                var abiString = JSON.stringify(result.abi);
-                var abiHash = crypto.createHash('sha256').update(abiString, 'utf8').digest('hex');
-                json += "\nvar " + result.name + " = {address: '" + result.address + "', \n" +
-                    "abi: " + abiString +", \n" +
-                    "abiHash: '"+ abiHash +"'};";
+                json += "\nvar " + result.name + " = " + JSON.stringify (
+                        {
+                            address: result.address,
+                            abi: result.abi,
+                            bytecode: compiledContracts[result.name].code
+                        }) + ";";
                 exports += ", " + result.name;
             } else {
-                console.log(result.reason);
+                console.log (result.reason);
             }
         });
-        exports = exports.replace("x, ", "");
-        console.log(json + "\n" + exports + "}");
+        exports = exports.replace ("x, ", "");
+        console.log (json + "\n" + exports + "}");
         return json + "\n" + exports + "}";
-    }).then(function(javascript) {
-        fs.writeFile("src/modules/ethereum/contract-definitions.js", javascript, (err) => {
-            if (err) throw err;
-            console.log('It\'s saved!');
-        });
+    }).then (function (javascript) {
+    fs.writeFile ("src/modules/ethereum/contract-definitions.js", javascript, (err) => {
+        if (err) throw err;
+        console.log ('It\'s saved!');
     });
+});
 
