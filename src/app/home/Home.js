d2c6cc2a79882d80017ff70b5686c29bb36b04c7
Lukasz Zygmanski <lukasz_zygmanski@epam.com>
2017-01-10 16:18:05 +0100

    home

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -4,6 +4,7 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
 import HomeController from './controllers/HomeController';
+
 import HelloComponent from './components/hello/HelloComponent';
 
 const home = 'app.home';
@@ -20,6 +21,52 @@ angular.module(home, [
             template: require('./templates/HomeTemplate.html'),
             controller: ('HomeController', HomeController)
         });
+
+        $stateProvider.state({
+            name: `${home}.undefined`,
+            url: 'dashboard',
+            template: require('./templates/HomeUndefinedTemplate.html'),
+            data: {
+                permissions: {
+                    only: ['UNDEFINED'],
+                    redirectTo: home
+                }
+            }
+        });
+        $stateProvider.state({
+            name: `${home}.retailer`,
+            url: 'dashboard-retailer',
+            template: require('./templates/HomeRetailerTemplate.html'),
+            data: {
+                permissions: {
+                    only: ['RETAILER'],
+                    redirectTo: home
+                }
+            }
+        });
+        $stateProvider.state({
+            name: `${home}.insurance`,
+            url: 'dashboard-insurance',
+            template: require('./templates/HomeInsuranceTemplate.html'),
+            data: {
+                permissions: {
+                    only: ['INSURANCE'],
+                    redirectTo: home
+                }
+            }
+        });
+        $stateProvider.state({
+            name: `${home}.owner`,
+            url: 'dashboard-owner',
+            template: require('./templates/HomeOwnerTemplate.html'),
+            data: {
+                permissions: {
+                    only: ['OWNER'],
+                    redirectTo: home
+                }
+            }
+        });
+
     }]);
 
 export default home
--- src/app/home/controllers/HomeController.js
+++ src/app/home/controllers/HomeController.js
@@ -1,4 +1,21 @@
-function HomeController($scope) {
-    $scope.title = 'hello, this is home page!'
+function HomeController($scope, $rootScope, $state, UserService) {
+    switch($rootScope.userRole) {
+        case 0 : {
+            $state.go('app.home.undefined');
+            return;
+        }
+        case 1 : {
+            $state.go('app.home.retailer');
+            return;
+        }
+        case 2 : {
+            $state.go('app.home.insurance');
+            return;
+        }
+        case 3 : {
+            $state.go('app.home.owner');
+            return;
+        }
+    }
 }
-export default ['$scope', HomeController]
+export default ['$scope', '$rootScope', '$state', 'UserService', HomeController]
