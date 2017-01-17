2eb685474058d68b46318cdfe38499926628365f
Lukasz Zygmanski <lukasz_zygmanski@epam.com>
2017-01-17 16:34:13 +0100

    loader fix

--- src/app/shared/services/TransactionService.js
+++ src/app/shared/services/TransactionService.js
@@ -1,5 +1,32 @@
 function TransactionService ($rootScope, $mdDialog, $compile, $state, $interval) {
 
+    let showInfo = (info, reload) => {
+        $mdDialog.show(
+            $mdDialog.alert({
+                onRemoving: () => {
+                    if(reload) {
+                        $state.reload();
+                    }
+                }
+            })
+                .clickOutsideToClose(true)
+                .title('Transaction Result')
+                .textContent(info)
+                .ariaLabel('Transaction Result')
+                .ok('Ok')
+        );
+    };
+
+    let actionsTranasction = (info = false, reload = false, log = false) => {
+        $rootScope.showMainLoader = false;
+        if(info) {
+            showInfo(info, reload);
+        }
+        if(log) {
+            consoel.log(log)
+        }
+    };
+
     this.showWidgetLoader = (elem, scope) => {
         let loaderElement = angular.element(`<div class="widget-loader" layout="row" layout-sm="column" layout-align="space-around">
             <md-progress-circular md-mode="indeterminate"></md-progress-circular>
@@ -13,23 +40,11 @@ function TransactionService ($rootScope, $mdDialog, $compile, $state, $interval)
         loader.remove();
     };
 
-
     this.startTransaction = () => {
         $rootScope.showMainLoader = true;
     };
-    function showInfo(info) {
-        $mdDialog.show(
-            $mdDialog.alert({})
-                .clickOutsideToClose(true)
-                .title('Transaction Result')
-                .textContent(info)
-                .ariaLabel('Transaction Result')
-                .ok('Ok')
-        );
-    }
 
     this.finishTransaction = (info = false, reload = false, log = false) => {
-
         if(reload) {
             let attempts = 0;
             let stopVerify = null;
@@ -38,33 +53,21 @@ function TransactionService ($rootScope, $mdDialog, $compile, $state, $interval)
                 web3.eth.getTransactionReceipt(info, function(err,res) {
                     if(err){
                         $interval.cancel(stopVerify);
-                        showInfo('error while executing the transaction ' + info + ':' + err);
+                        showInfo(`Error while executing the transaction ${info}: ${err}`);
                     }else {
                         if(res) {
-                            showInfo('transaction ' + info + " successful");
+                            actionsTranasction(`transaction ${info} successful`, true, log);
                             $interval.cancel(stopVerify);
-                            $rootScope.showMainLoader = false;
-                            $state.reload();
                         } else if(attempts > 600) {
-                            showInfo('transaction ' + info + " was needed included after 5 minutes");
+                            showInfo('Transaction ' + info + " was needed included after 5 minutes");
                             $interval.cancel(stopVerify);
-                            $rootScope.showMainLoader = false;
-                            $state.reload();
                         }
                     }
                 });
             };
             stopVerify = $interval(verifyTx, 1000);
         }else {
-            $rootScope.showMainLoader = false;
-
-            if(info) {
-                showInfo(info);
-            }
-
-            if(log) {
-                consoel.log(log)
-            }
+            actionsTranasction(info, log)
         }
     };
 
